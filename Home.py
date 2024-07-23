e08c3a8837571b973df28fdb3e9b8487ace64251
Aba Lőrincz, MD, PhD, MSc <114341000+Weston0793@users.noreply.github.com>
2024-07-23 19:24:56 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -8,6 +8,7 @@ from helper_functions import (
     select_sub_sub_sub_subregion, select_finger, select_complications, 
     select_associated_conditions, ao_classification, neer_classification, gartland_classification
 )
+
 def initialize_home_session_state():
     if 'confirm_data' not in st.session_state:
         st.session_state.confirm_data = None
