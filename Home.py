1df0d9ba36f328fb19fb6724190efe811e25cb08
Aba Lőrincz, MD, PhD, MSc <114341000+Weston0793@users.noreply.github.com>
2024-07-24 00:37:37 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -8,6 +8,7 @@ from helper_functions import (
     select_sub_sub_sub_subregion, select_finger, select_complications, 
     select_associated_conditions, ao_classification, neer_classification, gartland_classification
 )
+
 def initialize_home_session_state():
     if 'confirm_data' not in st.session_state:
         st.session_state.confirm_data = None
@@ -54,7 +55,7 @@ def main():
             col3, col4, col5 = st.columns([1, 1, 1])
             with col3:
                 if region['editable']:
-                    region['main_region'] = select_main_region(key=f"main_region_{idx}")
+                    region['main_region'] = st.selectbox("Fő régió", ["Felső végtag", "Alsó végtag", "Gerinc", "Koponya", "Mellkas", "Has"], key=f"main_region_{idx}")
                 else:
                     st.write(f"Fő régió: {region['main_region']}")
             if region['main_region']:
@@ -66,32 +67,32 @@ def main():
                             st.write(f"Oldal: {region['side']}")
                 if region['editable']:
                     with col5:
-                        region['sub_region'] = select_subregion(region['main_region'], key=f"sub_region_{idx}")
+                        region['sub_region'] = st.selectbox("Alrégió", ["Váll", "Humerus", "Könyök", "Alkar", "Csukló", "Kéz", "Medence", "Csípő", "Femur", "Térd", "Lábszár", "Boka", "Láb", "Gerinc", "Koponya", "Mellkas", "Has"], key=f"sub_region_{idx}")
                 else:
                     st.write(f"Alrégió: {region['sub_region']}")
             if region['sub_region']:
                 col6, col7, col8, col9 = st.columns([1, 1, 1, 1])
                 with col6:
                     if region['editable']:
-                        region['sub_sub_region'] = select_sub_subregion(region['sub_region'], key=f"sub_sub_region_{idx}")
+                        region['sub_sub_region'] = st.selectbox("Részletes régió", ["Részletek", "Egyéb"], key=f"sub_sub_region_{idx}")
                     else:
                         st.write(f"Részletes régió: {region['sub_sub_region']}")
                 if region['sub_sub_region']:
                     with col7:
                         if region['editable']:
-                            region['sub_sub_sub_region'] = select_sub_sub_subregion(region['sub_sub_region'], key=f"sub_sub_sub_region_{idx}")
+                            region['sub_sub_sub_region'] = st.selectbox("Legpontosabb régió", ["Legpontosabb", "Egyéb"], key=f"sub_sub_sub_region_{idx}")
                         else:
                             st.write(f"Legpontosabb régió: {region['sub_sub_sub_region']}")
                 if region['sub_sub_sub_region']:
                     with col8:
                         if region['editable']:
-                            region['sub_sub_sub_sub_region'] = select_sub_sub_sub_subregion(region['sub_sub_sub_region'], key=f"sub_sub_sub_sub_region_{idx}")
+                            region['sub_sub_sub_sub_region'] = st.selectbox("Legrészletesebb régió", ["Legrészletesebb", "Egyéb"], key=f"sub_sub_sub_sub_region_{idx}")
                         else:
                             st.write(f"Legrészletesebb régió: {region['sub_sub_sub_sub_region']}")
                     with col9:
                         if region['editable']:
                             if region['sub_sub_sub_region'] in ["Metacarpus", "Phalanx", "Metatarsus", "Lábujjak", "Pollex", "Hallux"]:
-                                region['finger'], _ = select_finger(region['sub_sub_sub_region'], key=f"finger_{idx}")
+                                region['finger'] = st.selectbox("Ujj", ["I", "II", "III", "IV", "V"], key=f"finger_{idx}")
                             else:
                                 region['finger'] = None
                         else:
