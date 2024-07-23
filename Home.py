af130393e65fb0c599d6d1ea922c645a55666f04
Aba Lőrincz, MD, PhD, MSc <114341000+Weston0793@users.noreply.github.com>
2024-07-24 00:03:03 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -54,44 +54,44 @@ def main():
             col3, col4, col5 = st.columns([1, 1, 1])
             with col3:
                 if region['editable']:
-                    region['main_region'] = select_main_region(key=f"main_region_{idx}")
+                    region['main_region'] = select_main_region()
                 else:
                     st.write(f"Fő régió: {region['main_region']}")
             if region['main_region']:
                 if region['main_region'] in ["Felső végtag", "Alsó végtag"]:
                     with col4:
                         if region['editable']:
-                            region['side'] = st.selectbox("Oldal", ["Bal", "Jobb"], index=["Bal", "Jobb"].index(region['side']) if region.get('side') else 0, key=f"side_{idx}")
+                            region['side'] = st.selectbox("Oldal", ["Bal", "Jobb"], index=["Bal", "Jobb"].index(region['side']) if region.get('side') else 0)
                         else:
                             st.write(f"Oldal: {region['side']}")
                 if region['editable']:
                     with col5:
-                        region['sub_region'] = select_subregion(region['main_region'], key=f"sub_region_{idx}")
+                        region['sub_region'] = select_subregion(region['main_region'])
                 else:
                     st.write(f"Alrégió: {region['sub_region']}")
             if region['sub_region']:
                 col6, col7, col8, col9 = st.columns([1, 1, 1, 1])
                 with col6:
                     if region['editable']:
-                        region['sub_sub_region'] = select_sub_subregion(region['sub_region'], key=f"sub_sub_region_{idx}")
+                        region['sub_sub_region'] = select_sub_subregion(region['sub_region'])
                     else:
                         st.write(f"Részletes régió: {region['sub_sub_region']}")
                 if region['sub_sub_region']:
                     with col7:
                         if region['editable']:
-                            region['sub_sub_sub_region'] = select_sub_sub_subregion(region['sub_sub_region'], key=f"sub_sub_sub_region_{idx}")
+                            region['sub_sub_sub_region'] = select_sub_sub_subregion(region['sub_sub_region'])
                         else:
                             st.write(f"Legpontosabb régió: {region['sub_sub_sub_region']}")
                 if region['sub_sub_sub_region']:
                     with col8:
                         if region['editable']:
-                            region['sub_sub_sub_sub_region'] = select_sub_sub_sub_subregion(region['sub_sub_sub_region'], key=f"sub_sub_sub_sub_region_{idx}")
+                            region['sub_sub_sub_sub_region'] = select_sub_sub_sub_subregion(region['sub_sub_sub_region'])
                         else:
                             st.write(f"Legrészletesebb régió: {region['sub_sub_sub_sub_region']}")
                     with col9:
                         if region['editable']:
                             if region['sub_sub_sub_region'] in ["Metacarpus", "Phalanx", "Metatarsus", "Lábujjak", "Pollex", "Hallux"]:
-                                region['finger'], _ = select_finger(region['sub_sub_sub_region'], key=f"finger_{idx}")
+                                region['finger'], _ = select_finger(region['sub_sub_sub_region'])
                             else:
                                 region['finger'] = None
                         else:
