c8e69daa69b80072110402f36928bdfba2029951
Aba Lőrincz, MD, PhD, MSc <114341000+Weston0793@users.noreply.github.com>
2024-07-24 23:51:55 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -72,7 +72,7 @@ def main():
                 st.session_state.regions.append(new_region)
                 st.success("Új régió hozzáadva")
                 st.session_state.new_region_blocked = True
-                st.rerun()
+                st.experimental_rerun()
             elif st.session_state.new_region_blocked:
                 st.error("Mentse a jelenlegi régiót mielőtt újat hozna létre.")
 
@@ -84,14 +84,14 @@ def main():
                 if st.button(f"Régió {idx + 1} mentése", key=f"save_region_{idx}"):
                     region['editable'] = False
                     st.session_state.new_region_blocked = False
-                    st.rerun()
+                    st.experimental_rerun()
             else:
                 if st.button(f"Régió {idx + 1} módosítása", key=f"modify_region_{idx}"):
                     region['editable'] = True
-                    st.rerun()
+                    st.experimental_rerun()
                 if st.button(f"Régió {idx + 1} törlése", key=f"delete_region_{idx}"):
                     st.session_state.regions.pop(idx)
-                    st.rerun()
+                    st.experimental_rerun()
 
     age = st.select_slider("Életkor (opcionális)", options=["NA"] + list(range(0, 121)), value="NA")
     age_group = ""
