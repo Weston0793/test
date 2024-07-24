6279ee962cf2d2b66b512c644e71b3b06a1bf73f
Aba Lőrincz, MD, PhD, MSc <114341000+Weston0793@users.noreply.github.com>
2024-07-24 23:46:18 +0200

    Update functions.py

--- functions.py
+++ functions.py
@@ -19,22 +19,27 @@ def initialize_home_session_state():
         st.session_state.multi_region = False
     if 'new_region_blocked' not in st.session_state:
         st.session_state.new_region_blocked = False
-    if 'uploaded_file' not in st.session_state:
-        st.session_state.uploaded_file = None
     if 'uploaded_files' not in st.session_state:
         st.session_state.uploaded_files = []
     if 'allow_multiple_uploads' not in st.session_state:
         st.session_state.allow_multiple_uploads = False
     if 'file_uploader_key' not in st.session_state:
         st.session_state.file_uploader_key = str(uuid.uuid4())
+        
+def display_images():
+    if not st.session_state.allow_multiple_uploads:
+        if st.session_state.uploaded_files:
+            st.image(st.session_state.uploaded_files[-1], caption=f"Feltöltött kép: {st.session_state.uploaded_files[-1].name}", use_column_width=True)
+    else:
+        cols = st.columns(2)
+        for idx, file in enumerate(st.session_state.uploaded_files):
+            with cols[idx % 2]:
+                st.image(file, caption=f"ID: {uuid.uuid4()} - {file.name}", use_column_width=True)
 
 def reset_session_state():
     for key in list(st.session_state.keys()):
         del st.session_state[key]
     initialize_home_session_state()
-
-def handle_file_upload(file):
-    return file
 
 def display_region(region, idx):
     col3, col4, col5 = st.columns([1, 1, 1])
