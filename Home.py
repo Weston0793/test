f10d1430a41d6fe6bbc16399371fca3887bdc829
Aba Lőrincz, MD, PhD, MSc <114341000+Weston0793@users.noreply.github.com>
2024-07-25 23:03:24 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -17,23 +17,24 @@ def main():
 
     #### 1. Kép feltöltése
     - Kérjük a feltöltésre szánt képekről bizonyosodjon meg hogy anonimizálva vannak! A képeken nem szerepelhet semmilyen betegazonosító!
-    - Válassza a bal oldalsáv "Navigáció" menüjéből a "Kép feltöltése" menüpontot .
-    - Adja meg a beteg azonosítóját és töltse fel a röntgenkép(ek)et (Max 15 MB).
+    - Válassza a bal oldalsáv "Navigáció" menüjéből a "Kép feltöltése" pontot.
+    - Húzza a "Drag and drop file here" mezőbe a képet vagy válassza ki a "Browse files" gombbal s töltse fel a röntgenkép(ek)et (Max 15 MB/file).
     - **Több kép feltöltése**: Ha egyszerre több képet szeretne feltölteni, pipálja ki a "Több kép feltöltése" lehetőséget. Figyelem: az összes kép ugyanazokat a címkéket kapja! (kivéve a betegazonosítót)
     - Kérem, adja meg a kötelező adatokat: korcsoport, röntgen nézet, normál vagy elváltozás típusa, melyik oldal (ha végtagról van szó), és a sérült régiók (fő régió, régió).
     - A részletesebb adatmegadás (alrégiók, komplikációk, társuló állapotok, osztályozások) nagyban segíti a kutatást és diagnosztikát.
     - **Több régió jelölése**: Ha több sérült régiót szeretne megadni, pipálja ki a "Több régió jelölése" opciót. Fontos: új régió hozzáadására csak mentés után van lehetőség, jelenlegi technikai korlátok miatt. Ha mentés nélkül több új régiót hozzáad, hibaüzenet fog keletkezni!
     - A választható súlyossági kategóriák folyamatosan bővülnek. A hosszú csöves csontoknál már elérhető a teljes AO klasszifikáció (a boka kívételével).
     - Kattintson a "Feltöltés" gombra a kíválasztott adatok újra összegzéséhez, majd a "Megerősítés és Feltöltés" gombbal véglegesítheti a feltöltést.
+    - Kérjük várja meg a zöld "Sikeres feltöltés" feliratot mielött új képet tölt fel. Több kép feltöltésénél egyszerre, többet kell várni.
 
     #### 2. Képek keresése
-    - Válassza a "Képek keresése" menüpontot.
+    - Válassza a "Képek keresése" menüpontot a bal oldalsáv "Navigáció" menüjéből.
     - Adja meg a keresési feltételeket (minimum: típus, nézet, főrégió).
     - Kattintson a "Keresés" gombra. A találatok listája megtekinthető és letölthető. 
     - Várjon egy pár másodpercet amíg a szerver összeállítja a "Letöltés" gomb megnyomása után a .zip filet, majd kattintson a "Megerősítés s Letöltés" gombra ha le kívánja tölteni a képeket és a hozzájuk tartozó címkéket. 
 
     #### 3. Státusz
-    - Válassza a "Státusz" menüpontot.
+    - Válassza a "Státusz" menüpontot a bal oldalsáv "Navigáció" menüjéből.
     - Tekintse meg a feltöltött röntgenképek statisztikáit és a projekt aktuális fázisának állapotát.
     - Az adatok alapján nyomon követheti a projekt előrehaladását és a hiányzó elemeket.
     - Az első fázis lezárási kirétirumai: az összes különböző csontot tartalmazó régióból, legalább két nézetből, normál és törött röntgenképeket gyüjts felnőttektől, kombinációnként legalább 50 darabot.
@@ -41,7 +42,7 @@ def main():
     - Fontos: egyelőre a státusz a gyermekkori röntgeneket is számba veszi!
 
     #### 4. Elérhetőség
-    - Válassza az "Elérhetőség" menüpontot.
+    - Válassza az "Elérhetőség" menüpontot a bal oldalsáv "Navigáció" menüjéből.
     - Ha bármilyen észrevétele van a honlappal kapcsolatban, segítségre van szüksége vagy kérdése van, lépjen nyugodtan kapcsolatba a fejlesztőkkel.
 
     ### Fontos Információk
