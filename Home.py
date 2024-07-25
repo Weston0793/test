f0b665cd4ef3ff028800eb23014d6f08c6518316
Aba Lőrincz, MD, PhD, MSc <114341000+Weston0793@users.noreply.github.com>
2024-07-25 04:17:27 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -5,46 +5,46 @@ def main():
 
     st.markdown("""
     ### Az alkalmazás célja
-    Az alkalmazás célja, hogy segítse az orvosi röntgenképek kezelését, feltöltését, keresését és állapotuk nyomon követését. A rendszer pontos és részletes adatokat gyűjt a röntgenképekről, hogy elősegítse az orvosi kutatást és a diagnosztikai munkát.
+    Az alkalmazás célja, hogy segítse az orvosi röntgenképek kezelését, feltöltését, keresését és állapotuk nyomon követését. A rendszer pontos és részletes adatokat gyűjt a röntgenképekről, hogy elősegítse az orvosi kutatást s okatatást valamint a traumatologiai diagnosztikai munkát, ezáltal javítva a betegek ellátását
 
     ### Funkciók
-    - **Kép feltöltése**: Új röntgenképeket tölthet fel az adatbázisba.
+    - **Kép feltöltése**: Új röntgenkép(ek)et tölthet fel az adatbázisba.
     - **Képek keresése**: Kereshet az adatbázisban található röntgenképek között különböző kritériumok alapján.
     - **Státusz**: Megtekintheti a feltöltött röntgenképek állapotát és statisztikáit.
-    - **Elérhetőség**: Kapcsolatba léphet a fejlesztőkkel vagy támogatást kérhet.
+    - **Elérhetőség**: Kapcsolatba léphet a fejlesztőkkel.
 
     ### Használati útmutató
 
     #### 1. Kép feltöltése
-    - Válassza a "Kép feltöltése" menüpontot.
-    - Adja meg a beteg azonosítóját és töltse fel a röntgenképet (Max 15 MB).
-    - **Több kép feltöltése**: Ha egyszerre több képet szeretne feltölteni, pipálja ki a "Több kép feltöltése" lehetőséget. Figyelem: az összes kép ugyanazokat a címkéket kapja!
+    - Kérjük a feltöltésre szánt képekről bizonyosodjon meg hogy anonimizálva vannak! A képeken nem szerepelhet semmilyen betegazonosító!
+    - Válassza a bal oldalsáv "Navigáció" menüjéből a "Kép feltöltése" menüpontot .
+    - Adja meg a beteg azonosítóját és töltse fel a röntgenkép(ek)et (Max 15 MB).
+    - **Több kép feltöltése**: Ha egyszerre több képet szeretne feltölteni, pipálja ki a "Több kép feltöltése" lehetőséget. Figyelem: az összes kép ugyanazokat a címkéket kapja! (kivéve a betegazonosítót)
     - Kérem, adja meg a kötelező adatokat: korcsoport, röntgen nézet, normál vagy elváltozás típusa, melyik oldal (ha végtagról van szó), és a sérült régiók (fő régió, alrégió).
     - A részletesebb adatmegadás (alrégiók, komplikációk, társuló állapotok, osztályozások) nagyban segíti a kutatást és diagnosztikát.
-    - **Több régió jelölése**: Ha több sérült régiót szeretne megadni, pipálja ki a "Több régió jelölése" opciót. Fontos: új régió hozzáadására csak mentés után van lehetőség a jelenlegi technikai korlátok miatt.
-    - Kattintson a "Feltöltés" gombra az adatok mentéséhez.
+    - **Több régió jelölése**: Ha több sérült régiót szeretne megadni, pipálja ki a "Több régió jelölése" opciót. Fontos: új régió hozzáadására csak mentés után van lehetőség, jelenlegi technikai korlátok miatt. Ha mentés nélkül több új régiót hozzáad, hibaüzenet fog keletkezni!
+    - Kattintson a "Feltöltés" gombra a kíválasztott adatok újra összegzéséhez, majd a "Megerősítés és Feltöltés" gombbal véglegesítheti a feltöltést.
 
     #### 2. Képek keresése
     - Válassza a "Képek keresése" menüpontot.
-    - Adja meg a keresési feltételeket (típus, nézet, régiók, komplikációk, társuló állapotok).
-    - Kattintson a "Keresés" gombra. A találatok listája megtekinthető és letölthető.
+    - Adja meg a keresési feltételeket (minimum: típus, nézet, főrégió).
+    - Kattintson a "Keresés" gombra. A találatok listája megtekinthető és letölthető. 
+    - Várjon egy pár másodpercet amíg a szerver összeállítja a "Letöltés" gomb megnyomása után a .zip filet, majd kattintson a "Megerősítés s Letöltés" gombra ha le kívánja tölteni a képeket és a hozzájuk tartozó címkéket. 
 
     #### 3. Státusz
     - Válassza a "Státusz" menüpontot.
-    - Tekintse meg a feltöltött röntgenképek statisztikáit és aktuális állapotát.
+    - Tekintse meg a feltöltött röntgenképek statisztikáit és a projekt aktuális fázisának állapotát.
     - Az adatok alapján nyomon követheti a projekt előrehaladását és a hiányzó elemeket.
+    - Az első fázis lezárási kirétirumai: az összes különböző csontot tartalmazó régióból, legalább két nézetből, normál és törött röntgenképeket gyüjtsön felnőttektől, kombinációnként legalább 50 darabot.
+    - Figyelem, egyelőre a státusz a gyermekkori röntgeneket is számba veszi!
 
     #### 4. Elérhetőség
     - Válassza az "Elérhetőség" menüpontot.
-    - Ha segítségre van szüksége vagy kérdése van, lépjen kapcsolatba a fejlesztőkkel.
-
-    ### Első fázis célja
-    Az első fázis célja, hogy egy átfogó adatbázist hozzunk létre a röntgenképekről, amely segíti az orvosi kutatásokat és diagnosztikát. A pontos és részletes adatmegadás lehetővé teszi az elemzések hatékonyságának növelését és a betegségek jobb megértését.
+    - Ha bármilyen észrevétele van a honlappal kapcsolatban, segítségre van szüksége vagy kérdése van, lépjen nyugodtan kapcsolatba a fejlesztőkkel.
 
     ### Fontos Információk
     - **Adatbiztonság**: Az összes feltöltött adat biztonságos és titkosított környezetben kerül tárolásra.
-    - **Adatvédelmi Szabályok**: Az alkalmazás megfelel az adatvédelmi jogszabályoknak, és az Ön adatait csak az Ön beleegyezésével használjuk fel.
-    - **Frissítések és Karbantartás**: Az alkalmazás rendszeresen frissül, hogy biztosítsa a legújabb funkciók és biztonsági intézkedések elérhetőségét.
+    - **Frissítések és Karbantartás**: Az alkalmazás rendszeresen frissül, hogy biztosítsa a legújabb és kényelmesebb funkciók és biztonsági intézkedéseket.
 
     Köszönjük, hogy használja az alkalmazásunkat!
     """)
