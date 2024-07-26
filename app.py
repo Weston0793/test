594100dfdf3b46123501951b446e2ddac252b502
Aba Lőrincz, MD, PhD, MSc <114341000+Weston0793@users.noreply.github.com>
2024-07-26 03:04:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,23 @@ from Search import search_section as search
 from Status import main as status
 from Contact import main as contact
 from Home import main as home
-
+# CSS stílus a navigációs sávhoz
+st.markdown("""
+    <style>
+        .sidebar .sidebar-content {
+            background-color: #f8f9fa;
+            padding: 20px;
+            border: 2px solid #000;
+            border-radius: 10px;
+        }
+        .sidebar .sidebar-content .selectbox {
+            font-size: 18px;
+        }
+        .sidebar .sidebar-content .stSelectbox {
+            margin-bottom: 20px;
+        }
+    </style>
+    """, unsafe_allow_html=True)
 app = MultiPage()
 
 # Add all your applications (pages) here with emojis as icons
