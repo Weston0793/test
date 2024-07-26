3e5c52332e2c2d26eca5308fd110e435ad02e444
Aba Lőrincz, MD, PhD, MSc <114341000+Weston0793@users.noreply.github.com>
2024-07-26 03:23:02 +0200

    Update multipage.py

--- multipage.py
+++ multipage.py
@@ -1,30 +1,5 @@
 import streamlit as st
 
-# CSS stílus a navigációs sávhoz
-st.markdown("""
-    <style>
-        .sidebar .sidebar-content {
-            background-color: #f8f9fa;
-            padding: 20px;
-            border: 2px solid #000;
-            border-radius: 10px;
-        }
-        .sidebar .sidebar-content .stSelectbox {
-            font-size: 18px;
-            padding: 10px;
-        }
-        .sidebar .sidebar-content img {
-            max-width: 100%;
-            border-radius: 10px;
-            margin-bottom: 20px;
-        }
-        .css-1d391kg {  /* Selectbox label */
-            font-size: 18px !important;
-            font-weight: bold !important;
-        }
-    </style>
-    """, unsafe_allow_html=True)
-
 class MultiPage:
     def __init__(self):
         self.pages = []
@@ -38,7 +13,6 @@ class MultiPage:
 
     def run(self):
         selected_page = st.sidebar.selectbox(
-            st.sidebar.markdown()
             'Navigáció',
             self.pages,
             format_func=lambda page: f"{page['icon']} {page['title']}" if page['icon'] else page['title']
