345b450c4ec45fcabb9dc5d909a415b72770f766
Aba Lőrincz, MD, PhD, MSc <114341000+Weston0793@users.noreply.github.com>
2024-07-26 02:53:57 +0200

    Update multipage.py

--- multipage.py
+++ multipage.py
@@ -4,16 +4,19 @@ class MultiPage:
     def __init__(self):
         self.pages = []
 
-    def add_page(self, title, func):
+    def add_page(self, title, func, icon=None):
         self.pages.append({
             "title": title,
-            "function": func
+            "function": func,
+            "icon": icon
         })
 
     def run(self):
-        page = st.sidebar.selectbox(
-            'Navigáció',
+        selected_page = st.sidebar.selectbox(
+            'Navigation',
             self.pages,
             format_func=lambda page: page['title']
         )
-        page['function']()
+        st.title(selected_page['title'])
+        st.sidebar.markdown(f"# {selected_page['icon']} {selected_page['title']}")
+        selected_page['function']()
