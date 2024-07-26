fc503a816ba271855755400a4cf2c30082194e04
Aba Lőrincz, MD, PhD, MSc <114341000+Weston0793@users.noreply.github.com>
2024-07-26 02:34:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from multipage import MultiPage, nav_page
+from multipage import MultiPage
 from Upload import main as upload
 from Search import search_section as search
 from Status import main as status
@@ -14,12 +14,6 @@ app.add_page("Kép feltöltése", upload)
 app.add_page("Képek keresése", search)
 app.add_page("Státusz", status)
 app.add_page("Elérhetőség", contact)
-
-# Example usage of nav_page in buttons
-if st.button("< Prev"):
-    nav_page("Főoldal")
-if st.button("Next >"):
-    nav_page("Kép feltöltése")
 
 # The main app
 if __name__ == '__main__':
