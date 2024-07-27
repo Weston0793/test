2ec45f8ba967d32864a394f3b336877a370b0544
Aba Lőrincz, MD, PhD, MSc <114341000+Weston0793@users.noreply.github.com>
2024-07-27 03:13:33 +0200

    Update upload_backend.py

--- upload_backend.py
+++ upload_backend.py
@@ -13,7 +13,7 @@ def confirm_and_upload_data(upload_data):
         age_group = st.radio("Kérem válassza ki az életkori csoportot", ["Gyermek", "Felnőtt"])
         upload_data['age_group'] = age_group
 
-    st.markdown('<div class="confirmation-box">', unsafe_allow_html=True)
+    st.markdown('---')
     st.markdown('<div class="confirmation-title">Kérlek, a feltöltéshez erősítsd meg a következő adatokat:</div>', unsafe_allow_html=True)
 
     cols = st.columns(2)
@@ -48,57 +48,66 @@ def confirm_and_upload_data(upload_data):
         if upload_data["associated_conditions"]:
             st.markdown(f'**<span class="highlight">Társuló Kórállapotok:</span>** {", ".join(upload_data["associated_conditions"])}', unsafe_allow_html=True)
 
-    st.markdown("### Kiválasztott régiók", unsafe_allow_html=True)
+    st.markdown('### Kiválasztott régiók', unsafe_allow_html=True)
 
+    cols = st.columns(2)
     for idx, region in enumerate(upload_data["regions"]):
-        st.markdown(f"<div class='content'><strong><span class='highlight'>Régió {idx + 1}:</span></strong></div>", unsafe_allow_html=True)
-        if region['main_region']:
-            st.markdown(f"<div class='content'><strong><span class='highlight'>Fő régió:</span></strong> {region['main_region']}</div>", unsafe_allow_html=True)
-        if region['side']:
-            st.markdown(f"<div class='content'><strong><span class='highlight'>Oldal:</span></strong> {region['side']}</div>", unsafe_allow_html=True)
-        if region['sub_region']:
-            st.markdown(f"<div class='content'><strong><span class='highlight'>Alrégió:</span></strong> {region['sub_region']}</div>", unsafe_allow_html=True)
-        if region['sub_sub_region']:
-            st.markdown(f"<div class='content'><strong><span class='highlight'>Részletes régió:</span></strong> {region['sub_sub_region']}</div>", unsafe_allow_html=True)
-        if region['sub_sub_sub_region']:
-            st.markdown(f"<div class='content'><strong><span class='highlight'>Legpontosabb régió:</span></strong> {region['sub_sub_sub_region']}</div>", unsafe_allow_html=True)
-        if region['finger']:
-            st.markdown(f"<div class='content'><strong><span class='highlight'>Ujj:</span></strong> {region['finger']}</div>", unsafe_allow_html=True)
-        if region['sub_sub_sub_sub_region']:
-            st.markdown(f"<div class='content'><strong><span class='highlight'>Legrészletesebb régió:</span></strong> {region['sub_sub_sub_sub_region']}</div>", unsafe_allow_html=True)
+        col = cols[idx % 2]
+        with col:
+            st.markdown(f"**<span class='highlight'>Régió {idx + 1}:</span>**", unsafe_allow_html=True)
+            if region['main_region']:
+                st.markdown(f"**<span class='highlight'>Fő régió:</span>** {region['main_region']}", unsafe_allow_html=True)
+            if region['side']:
+                st.markdown(f"**<span class='highlight'>Oldal:</span>** {region['side']}", unsafe_allow_html=True)
+            if region['sub_region']:
+                st.markdown(f"**<span class='highlight'>Alrégió:</span>** {region['sub_region']}", unsafe_allow_html=True)
+            if region['sub_sub_region']:
+                st.markdown(f"**<span class='highlight'>Részletes régió:</span>** {region['sub_sub_region']}", unsafe_allow_html=True)
+            if region['sub_sub_sub_region']:
+                st.markdown(f"**<span class='highlight'>Legpontosabb régió:</span>** {region['sub_sub_sub_region']}", unsafe_allow_html=True)
+            if region['finger']:
+                st.markdown(f"**<span class='highlight'>Ujj:</span>** {region['finger']}", unsafe_allow_html=True)
+            if region['sub_sub_sub_sub_region']:
+                st.markdown(f"**<span class='highlight'>Legrészletesebb régió:</span>** {region['sub_sub_sub_sub_region']}", unsafe_allow_html=True)
 
-        if region.get("classification"):
-            for classification_name, details in region["classification"].items():
-                if "name" in details and "severity" in details:
-                    st.markdown(f"<div class='content'><strong><span class='highlight'>Osztályozás:</span></strong> {details['name']}</div>", unsafe_allow_html=True)
-                    st.markdown(f"<div class='content'><strong><span class='highlight'>Súlyosság:</span></strong> {details['severity']}</div>", unsafe_allow_html=True)
-                    if "subseverity" in details:
-                        st.markdown(f"<div class='content'><strong><span class='highlight'>Alsúlyosság:</span></strong> {details['subseverity']}</div>", unsafe_allow_html=True)
+            if region.get("classification"):
+                for classification_name, details in region["classification"].items():
+                    if "name" in details and "severity" in details:
+                        st.markdown(f"**<span class='highlight'>Osztályozás:</span>** {details['name']}", unsafe_allow_html=True)
+                        st.markdown(f"**<span class='highlight'>Súlyosság:</span>** {details['severity']}", unsafe_allow_html=True)
+                        if "subseverity" in details:
+                            st.markdown(f"**<span class='highlight'>Alsúlyosság:</span>** {details['subseverity']}", unsafe_allow_html=True)
 
-    st.markdown('</div>', unsafe_allow_html=True)
+    st.markdown('---')
 
-    if st.button("Megerősít és Feltölt", key="confirm_upload", help="Kattintson a feltöltés megerősítéséhez"):
-        try:
-            save_image(
-                patient_id=upload_data["patient_id"],
-                files=upload_data["files"],
-                main_type=upload_data["main_type"],
-                sub_type=upload_data["sub_type"],
-                sub_sub_type=upload_data["sub_sub_type"],
-                view=upload_data["view"],
-                sub_view=upload_data["sub_view"],
-                sub_sub_view=upload_data["sub_sub_view"],
-                gender=upload_data["gender"],
-                age=upload_data["age"],
-                age_group=upload_data["age_group"],
-                comment=upload_data["comment"],
-                complications=upload_data["complications"],
-                associated_conditions=upload_data["associated_conditions"],
-                regions=upload_data["regions"]
-            )
-            st.success("Kép sikeresen feltöltve!")
-            st.session_state["confirm_data"] = None
-            st.experimental_set_query_params(scroll_to="confirmation")
-        except Exception as e:
-            st.error(f"Hiba a kép mentésekor: {e}")
-            st.session_state["confirm_data"] = None
+    col1, col2, col3 = st.columns([1, 1, 1])
+    with col1:
+        st.write("")
+    with col2:
+        if st.button("Megerősít és Feltölt", key="confirm_upload", help="Kattintson a feltöltés megerősítéséhez"):
+            try:
+                save_image(
+                    patient_id=upload_data["patient_id"],
+                    files=upload_data["files"],
+                    main_type=upload_data["main_type"],
+                    sub_type=upload_data["sub_type"],
+                    sub_sub_type=upload_data["sub_sub_type"],
+                    view=upload_data["view"],
+                    sub_view=upload_data["sub_view"],
+                    sub_sub_view=upload_data["sub_sub_view"],
+                    gender=upload_data["gender"],
+                    age=upload_data["age"],
+                    age_group=upload_data["age_group"],
+                    comment=upload_data["comment"],
+                    complications=upload_data["complications"],
+                    associated_conditions=upload_data["associated_conditions"],
+                    regions=upload_data["regions"]
+                )
+                st.success("Kép sikeresen feltöltve!")
+                st.session_state["confirm_data"] = None
+                st.experimental_set_query_params(scroll_to="confirmation")
+            except Exception as e:
+                st.error(f"Hiba a kép mentésekor: {e}")
+                st.session_state["confirm_data"] = None
+    with col3:
+        st.write("")
