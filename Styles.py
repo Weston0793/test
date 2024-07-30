cb9b9208874810c51f1462868051c3f42805261b
Aba Lőrincz, MD, PhD, MSc <114341000+Weston0793@users.noreply.github.com>
2024-07-30 03:34:00 +0200

    Update Styles.py

--- Styles.py
+++ Styles.py
@@ -453,13 +453,13 @@ def status_markdown():
                 transform: scale(1);
             }
             18% {
-                transform: scale(1.05); /* Peak of diastole */
+                transform: scale(1); /* Peak of diastole */
             }
             25% {
-                transform: scale(1); /* End of atrial systole */
+                transform: scale(0.9); /* End of atrial systole */
             }
             40% {
-                transform: scale(1); /* Peak of ventricular systole */
+                transform: scale(0.7); /* Peak of ventricular systole */
             }
             55% {
                 transform: scale(0.9); /* End of ventricular systole */
